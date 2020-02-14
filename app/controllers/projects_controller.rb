7c6eb0dfd24f684454af0cf5f70b142b4527d9e7
“hvasquez0” <hvasquez0@live.com>
2020-02-14 14:40:10 -0500

    fixed login form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,4 @@
+
 class ProjectsController < ApplicationController
 	def index
         @projects = Project.all
@@ -23,6 +24,7 @@ class ProjectsController < ApplicationController
     def show
         @project = Project.find(params[:id])
     end
+    
     
     def destroy
         project =Project.find(params[:id])
