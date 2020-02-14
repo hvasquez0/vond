553dadb15bc2ab28c676ae7832f66810dc0312c3
“hvasquez0” <hvasquez0@live.com>
2020-02-14 15:47:52 -0500

    fix migrations

--- db/migrate/20200210195810_create_users.rb
+++ db/migrate/20200210195810_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
 
     	t.string :username
     	t.string :email
-    	t.integer :phone
+    	t.string :phone
     	t.string :profession
     	t.integer :experience_level
     	t.text :further_information
