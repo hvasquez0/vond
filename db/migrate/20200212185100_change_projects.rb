c0c568e8f267f53d4601c5772955bd7c8501ad91
“hvasquez0” <hvasquez0@live.com>
2020-02-14 14:56:33 -0500

    third fixed login form

--- db/migrate/20200212185100_change_projects.rb
+++ db/migrate/20200212185100_change_projects.rb
@@ -1,7 +1,6 @@
 class ChangeProjects < ActiveRecord::Migration[6.0]
   def change
-  	change_column :projects, :time_begin, :date
-  	change_column :projects, :time_end, :date
+
   	change_column :projects, :time_begin, 'date USING CAST(column_name AS date)'
   	change_column :projects, :time_end, 'date USING CAST(column_name AS date)'
 
