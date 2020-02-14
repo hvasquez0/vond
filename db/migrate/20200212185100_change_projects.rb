1e296ea4b935f25ab08a1b35d29e93a9ce38e029
“hvasquez0” <hvasquez0@live.com>
2020-02-14 15:23:55 -0500

    fourth

--- db/migrate/20200212185100_change_projects.rb
+++ db/migrate/20200212185100_change_projects.rb
@@ -1,8 +1,8 @@
 class ChangeProjects < ActiveRecord::Migration[6.0]
   def change
 
-  	change_column :projects, :time_begin, 'date USING CAST(column_name AS date)'
-  	change_column :projects, :time_end, 'date USING CAST(column_name AS date)'
+  	change_column :projects, :time_begin, 'date USING CAST(time_begin AS date)'
+  	change_column :projects, :time_end, 'date USING CAST(time_end AS date)'
 
 
   	change_column :users, :phone, :string
